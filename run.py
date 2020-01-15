078a3b6e8f9474fa9bbccdefebd0e266db643a82
alexandre <alexandre@paradisiak.com>
2020-01-15 13:48:23 +0100

    Fix wsgi

--- run.py
+++ run.py
@@ -3,7 +3,6 @@ from db import db
 
 db.init_app(app)
 
-
 @app.before_first_request
 def create_tables():
-    db.create_all()  # Crée les tables qu'il voit dans les imports de app
+    db.create_all()
